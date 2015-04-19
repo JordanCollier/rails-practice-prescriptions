87eb7220a935351b165c5121a68d315eee498aee
Jordan Collier <jordanicollier@gmail.com>
2015-04-19 17:09:05 -0600

    add relationship to presciption in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  has_many :prescriptions
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :name, presence: true, uniqueness: {case_sensitive: false}
 end
